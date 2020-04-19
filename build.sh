9f47e4d856c6d354d87507ce4bb8d60969e68b1c
y3tu <14643092@qq.com>
2020-04-19 16:39:11 +0800

    update

--- build.sh
+++ build.sh
@@ -35,42 +35,42 @@ cp ./yao-server/yao-support/yao-support-server/target/yao-support-server*.jar ${
 cp ./yao-server/yao-log/yao-log-server/target/yao-log-server*.jar ${JAR_DIR}
 
 
-echo -- -- 备份以前的镜像 -- --
-
-time=$(date "+%Y%m%d%H%M%S")
-docker tag yao_yao-auth:latest yao_yao-auth:$time
-docker tag yao_yao-gateway:latest yao_yao-gateway:$time
-docker tag yao_yao-upms-server:latest yao_yao-upms-server:$time
-docker tag yao_yao-support-server:latest yao_yao-support-server:$time
-docker tag yao_yao-log-server:latest yao_yao-log-server:$time
+#echo -- -- 备份以前的镜像 -- --
+#
+#time=$(date "+%Y%m%d%H%M%S")
+#docker tag yao_yao-auth:latest yao_yao-auth:$time
+#docker tag yao_yao-gateway:latest yao_yao-gateway:$time
+#docker tag yao_yao-upms-server:latest yao_yao-upms-server:$time
+#docker tag yao_yao-support-server:latest yao_yao-support-server:$time
+#docker tag yao_yao-log-server:latest yao_yao-log-server:$time
 
 
 echo -- -- run docker-compose build 创建镜像-- --
 docker-compose  -f ${COMPOSE_FILE} build
 
-echo -- -- 上传镜像-- --
-docker tag yao_yao-auth:latest y3tu/yao-auth:latest
-docker push y3tu/yao-auth:latest
-docker rmi y3tu/yao-auth:latest
-
-docker tag yao_yao-gateway:latest y3tu/yao-gateway:latest
-docker push y3tu/yao-gateway:latest
-docker rmi y3tu/yao-gateway:latest
-
-docker tag yao_yao-upms-server:latest y3tu/yao-upms-server:latest
-docker push y3tu/yao-upms-server:latest
-docker rmi y3tu/yao-upms-server:latest
-
-docker tag yao_yao-support-server:latest y3tu/yao-support-server:latest
-docker push y3tu/yao-support-server:latest
-docker rmi y3tu/yao-support-server:latest
-
-docker tag yao_yao-log-server:latest y3tu/yao-log-server:latest
-docker push y3tu/yao-log-server:latest
-docker rmi y3tu/yao-log-server:latest
-
-echo -- -- 上传镜像结束-- --
-
-
-docker images|grep docker|awk '{print $3 }'|xargs docker rmi
+#echo -- -- 上传镜像-- --
+#docker tag yao_yao-auth:latest y3tu/yao-auth:latest
+#docker push y3tu/yao-auth:latest
+#docker rmi y3tu/yao-auth:latest
+#
+#docker tag yao_yao-gateway:latest y3tu/yao-gateway:latest
+#docker push y3tu/yao-gateway:latest
+#docker rmi y3tu/yao-gateway:latest
+#
+#docker tag yao_yao-upms-server:latest y3tu/yao-upms-server:latest
+#docker push y3tu/yao-upms-server:latest
+#docker rmi y3tu/yao-upms-server:latest
+#
+#docker tag yao_yao-support-server:latest y3tu/yao-support-server:latest
+#docker push y3tu/yao-support-server:latest
+#docker rmi y3tu/yao-support-server:latest
+#
+#docker tag yao_yao-log-server:latest y3tu/yao-log-server:latest
+#docker push y3tu/yao-log-server:latest
+#docker rmi y3tu/yao-log-server:latest
+#
+#echo -- -- 上传镜像结束-- --
+#
+#
+#docker images|grep docker|awk '{print $3 }'|xargs docker rmi
 
